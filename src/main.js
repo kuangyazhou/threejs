c6e1b1de20e8482484059aa645fcd1b3eca8ad5e
况亚洲 <18707174713@139.com>
2018-12-11 11:15:24 +0800

    points

--- src/main.js
+++ src/main.js
@@ -1,12 +1,43 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.prototype.$randomColor = function() {
+  var arrHex = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f"
+    ],
+    strHex = "0x",
+    index;
+
+  for (var i = 0; i < 6; i++) {
+    index = Math.round(Math.random() * 15);
+
+    strHex += arrHex[index];
+  }
+
+  return strHex;
+};
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/views/sprite.vue
+++ src/views/sprite.vue
@@ -31,14 +31,24 @@ export default {
 
       this.scene = new THREE.Scene();
 
-      // this.camera = new THREE.PerspectiveCamera(
-      //   45,
-      //   window.innerWidth / window.innerHeight,
+      // 透视相机
+      this.camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        1,
+        10000
+      );
+
+      // 正交相机
+      // this.camera = new THREE.OrthographicCamera(
+      //   window.innerWidth / -2,
+      //   window.innerWidth / 2,
+      //   window.innerHeight / 2,
+      //   window.innerHeight / -2,
       //   1,
-      //   10000
+      //   1000
       // );
-      this.camera = new THREE.OrthographicCamera(-2000, 2000, 2000, -20000, 1, 1000);
-      this.camera.position.set(100, 100, 100);
+      this.camera.position.set(300, 300, 500);
       this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
       this.ambientLight = new THREE.AmbientLight(0x404040);
@@ -83,25 +93,64 @@ export default {
         for (let y = -5; y <= 5; y++) {
           for (let z = -5; z <= 5; z++) {
             let sprite = new THREE.Sprite(material);
-            // for (let z = -5; z <= -5; z++) {
             sprite.position.set(x * 10, y * 10, z * 10);
             this.scene.add(sprite);
-            // console.log(sprite);
-            // }
           }
         }
-        // for (let z = -5; z <= 5; z++) {
-        //   let fuck = new THREE.Sprite(material);
-        //   fuck.position.set(x * 10, 0, z * 10);
-        //   this.scene.add(fuck);
-        // }
       }
+
+      let geom = new THREE.Geometry();
+      let points = new THREE.PointCloudMaterial({
+        size: 4,
+        vertexColors: true,
+        color: 0xffffff
+      });
+
+      for (let x = 10; x <= 20; x++) {
+        for (let y = 10; y <= 20; y++) {
+          for (let z = 10; z <= 20; z++) {
+            let particle = new THREE.Vector3(x * 10, y * 10, z * 10);
+            geom.vertices.push(particle);
+            geom.colors.push(new THREE.Color(+this.randomColor()));
+          }
+        }
+      }
+      let cloud = new THREE.PointCloud(geom, points);
+      this.scene.add(cloud);
     },
     render() {
       this.renderer.render(this.scene, this.camera);
       this.controls.update();
       requestAnimationFrame(this.render);
+    },
+    randomColor() {
+      var arrHex = [
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "a",
+          "b",
+          "c",
+          "d",
+          "e",
+          "f"
+        ],
+        strHex = "0x",
+        index;
+      for (var i = 0; i < 6; i++) {
+        index = Math.round(Math.random() * 15);
+        strHex += arrHex[index];
+      }
+      return strHex;
     }
-  }
+  },
+  destroyed() {}
 };
 </script>
